e8a8c2e66e9aacd03e42601a3d2976ee78c1b5a3
Firzan Armani <mrfirzanarmani@gmail.com>
2024-08-04 23:42:32 +0800

    update API entry point listening callback

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -6,11 +6,11 @@ const port = process.env.PORT ? parseInt(process.env.PORT) : 5001;
 const app = createServer();
 
 // Adjust below based on choice of runtime and/or deployment. By default, we are using Node.js
+
 serve({
   fetch: app.fetch,
-  port,
+}).listen({ port }, () => {
+  log(`api running on ${port}`);
 });
-
-log(`api running on ${port}`);
 
 export type AppType = typeof app;
