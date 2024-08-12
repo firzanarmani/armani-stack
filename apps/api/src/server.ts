aa70d3daea28006352286bbfebc8a6d566b4ebc7
Firzan Armani <mrfirzanarmani@gmail.com>
2024-08-12 12:18:40 +0800

    setup cloudflare deployment and wrangler

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,9 +1,10 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
+import { type HonoAppType } from "./types/hono";
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type -- Take advantage of the Hono's type inferencing for routes
 export const createServer = () => {
-  const app = new Hono()
+  const app = new Hono<HonoAppType>()
     .use("/*", cors())
     .get("/message/:name", (c) => {
       return c.json({ message: `hello ${c.req.param("name")}` });
