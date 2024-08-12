48d8b454dc8f180dca06d6295990e4ef17566f96
Firzan Armani <mrfirzanarmani@gmail.com>
2024-08-12 12:28:17 +0800

    change app to use the correct port for dev

--- apps/admin/src/lib/api.ts
+++ apps/admin/src/lib/api.ts
@@ -1,4 +1,4 @@
 import { type AppType } from "api";
 import { hc } from "hono/client";
 
-export const api = hc<AppType>("http://localhost:5001");
+export const api = hc<AppType>(import.meta.env.VITE_API_URL);
