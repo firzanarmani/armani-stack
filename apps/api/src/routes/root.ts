e844106eac1865a11f653d5b746d5ecec2bd48a3
Firzan Armani <mrfirzanarmani@gmail.com>
2024-08-22 17:37:10 +0800

    refactor api routes as separate routers

---
+++ apps/api/src/routes/root.ts
@@ -0,0 +1,34 @@
+import { Hono } from "hono";
+import { drizzle } from "drizzle-orm/d1";
+import { type HonoAppType } from "../types/hono";
+import * as schema from "../db/schema";
+
+export const RootRouter = new Hono<HonoAppType>()
+  .get("/message/:name", async (c) => {
+    const db = drizzle(c.env.DB, { schema });
+
+    const name = await db.query.namesTable.findFirst({
+      where: (names, { eq }) => eq(names.name, c.req.param("name")),
+    });
+
+    if (name)
+      return c.json({ message: `Welcome back ${c.req.param("name")}.` });
+
+    try {
+      const newName = await db
+        .insert(schema.namesTable)
+        .values({ name: c.req.param("name") })
+        .returning();
+
+      return c.json({
+        message: `Hello ${newName[0].name}.`,
+      });
+    } catch (error) {
+      return c.json({
+        message: `Unable to create a new user named ${c.req.param("name")}.`,
+      });
+    }
+  })
+  .get("/status", (c) => {
+    return c.json({ ok: true });
+  });
--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,41 +1,11 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
-import { drizzle } from "drizzle-orm/d1";
 import { type HonoAppType } from "./types/hono";
-import * as schema from "./db/schema";
+import { RootRouter } from "./routes/root";
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type -- Take advantage of the Hono's type inferencing for routes
 export const createServer = () => {
-  const app = new Hono<HonoAppType>()
-    .use("/*", cors())
-    .get("/message/:name", async (c) => {
-      const db = drizzle(c.env.DB, { schema });
-
-      const name = await db.query.namesTable.findFirst({
-        where: (names, { eq }) => eq(names.name, c.req.param("name")),
-      });
-
-      if (name)
-        return c.json({ message: `Welcome back ${c.req.param("name")}.` });
-
-      try {
-        const newName = await db
-          .insert(schema.namesTable)
-          .values({ name: c.req.param("name") })
-          .returning();
-
-        return c.json({
-          message: `Hello ${newName[0].name}.`,
-        });
-      } catch (error) {
-        return c.json({
-          message: `Unable to create a new user named ${c.req.param("name")}.`,
-        });
-      }
-    })
-    .get("/status", (c) => {
-      return c.json({ ok: true });
-    });
+  const app = new Hono<HonoAppType>().use("/*", cors()).route("/", RootRouter);
 
   return app;
 };
